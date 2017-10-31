1ad587cb1ad73a3845f8a35cd9fe7b4573c50e7f
John Tuttle <thejwt@gmail.com>
2017-10-31 20:51:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,15 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @team_name = params[:team_name]
+    @coach = params[:coach]
+    @point_guard = params[:point_guard]
+    @shooting_guard = params[:shooting_guard]
+    @power_forward = params[:power_forward]
+    @small_forward = params[:small_forward]
+    @center = params[:center]
 
+    erb :team
   end
 
 end
